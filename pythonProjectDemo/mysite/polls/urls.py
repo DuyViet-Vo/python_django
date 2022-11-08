b761dbcb546c73c8ad66175cd48ca47bdc0c559c
DuyViet-Vo <duyviet1810@gmail.com>
2022-11-08 15:46:29 +0700

    template part3

--- pythonProjectDemo/mysite/polls/urls.py
+++ pythonProjectDemo/mysite/polls/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("list/", views.viewList, name="View_list"),
     path('',views.index,name= "index" ),
 ]
--- pythonProjectDemo/mysite/polls/views.py
+++ pythonProjectDemo/mysite/polls/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import *
 # Create your views here.
+
+
 def index(request):
     myname = 'Võ Duy Việt'
-    taisan=['Điện Thoại','Máy tính','xe ware']
-    context ={"name": myname,'possession': taisan}
-    return render(request,"polls/index.html",context)
+    taisan = ['Điện Thoại', 'Máy tính', 'xe ware']
+    context = {"name": myname, 'possession': taisan}
+    return render(request, "polls/index.html", context)
+
+def viewList(request):
+    list_question = Question.objects.all()
+    context1 = {"dsquestions": list_question}
+    return render(request, "polls/question_list.html", context1)
