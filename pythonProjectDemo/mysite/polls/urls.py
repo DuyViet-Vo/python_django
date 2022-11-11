d7507a6c4f324f2c0faf71a302469d02fd5efdf4
DuyViet-Vo <duyviet1810@gmail.com>
2022-11-11 15:17:32 +0700

    bai 10

--- pythonProjectDemo/mysite/polls/urls.py
+++ pythonProjectDemo/mysite/polls/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("detail/<int:question_id>", views.detailView, name="detail"),
     path("list/", views.viewList, name="View_list"),
     path('',views.index,name= "index" ),
+
 ]
--- pythonProjectDemo/mysite/polls/views.py
+++ pythonProjectDemo/mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from .models import *
 # Create your views here.
 
@@ -19,4 +19,5 @@ def viewList(request):
 
 def detailView(request, question_id):
     q = Question.objects.get(pk=question_id)
-    return render(request, "polls/detail_question.html", {"qs": q})
+    return render(request, "polls/detail_question.html", {"questionpk": q})
+    
