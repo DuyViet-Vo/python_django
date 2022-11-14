0a489f85bc361344a0a4ae96f95e8078878eccb4
DuyViet-Vo <duyviet1810@gmail.com>
2022-11-14 21:02:23 +0700

    class base view

--- pythonProjectFrom/demoform/news/urls.py
+++ pythonProjectFrom/demoform/news/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from . import views
 app_name = 'news'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('adds/', views.add_post, name='add'),
-    path('save/', views.save_news, name='save'),
+    path('', views.IndexClass.as_view(), name='index'),
+    path('adds/', views.PostClass.as_view(), name='add'),
+    path('save/', views.ClassSaveNews.as_view(), name='save'),
     path('email/',views.email_view,name ='email'),
     path('process',views.process,name ='process'),
 ]
--- pythonProjectFrom/demoform/news/views.py
+++ pythonProjectFrom/demoform/news/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import *
+from django.views import View
 from django.template.response import TemplateResponse
 # Create your views here.
-def index(request):
-    return HttpResponse('xin chao!')
+class IndexClass(View):
+    def get(self,request):
+        ketqua= '123123asfdgsa'
+        return HttpResponse(ketqua)
+class PostClass(View):
+    def get(self,request):
+        a = PostForm()
+        return render(request,"news/add_news.html",{'f': a})
 
-def add_post(request):
-    a = PostForm()
-    return render(request,"news/add_news.html",{'f': a})
-
-def save_news(request):
-    if request.method == "POST":
+class ClassSaveNews(View):
+    def post(self,request):
         g = PostForm(request.POST)
         if g.is_valid():
             g.save()
             return  HttpResponse('Luw Thanh Cong!')
         else:
             return HttpResponse('Loi Validate')
-    else:
-        return HttpResponse('Khong phai POST request!')
 
 def email_view(requset):
     b = SendEmail()
